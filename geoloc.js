456dff4cb52da6b290b02b5de072b82188bc09a8
Paulo <olivegeoarte@gmail.com>
2013-10-11 08:28:53 -0300

    denovo

--- geoloc.js
+++ geoloc.js
@@ -4,6 +4,9 @@
 	function inicia (){
 		document.addEventListener("deviceready", onDeviceReady, false);
 		var watchID = null;
+		
+		
+		alert (element.innerHTML());
 		}
 	
     
@@ -21,7 +24,7 @@
         var element = document.getElementById('geolocation');
         element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
                             'Longitude: '          + position.coords.longitude             + '<br />' +
-                            /*'Altitude: '           + position.coords.altitude              + '<br />' +
+                            /*'Altitude: '         + position.coords.altitude              + '<br />' +
                             'Accuracy: '           + position.coords.accuracy              + '<br />' +
                             'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
                             'Heading: '            + position.coords.heading               + '<br />' +
